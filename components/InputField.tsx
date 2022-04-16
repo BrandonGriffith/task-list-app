3bd54771cc4ce9c7cf8d972e241a4839558ff3aa
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-04-16 14:44:12 -0400

    added input box

---
+++ components/InputField.tsx
@@ -0,0 +1,9 @@
+const InputField = () => {
+    return (
+    <form className="input">
+        <input type="input" className="input-box" placeholder="Enter a task here..."/>
+        <button type="submit" className="input-btn">Add Task</button>
+    </form>
+    )
+}
+export default InputField
